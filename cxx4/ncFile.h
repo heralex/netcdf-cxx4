35c228000598ca5eb5ebf3accec60b053690361c
Alex Herzig <herziga@landcareresearch.co.nz>
2022-01-05 16:38:28 +1300

    enh: ncFile - add ctor and open/create methods for parallel I/O

--- cxx4/ncFile.h
+++ cxx4/ncFile.h
@@ -2,6 +2,9 @@
 #include "ncGroup.h"
 #include "netcdf.h"
 
+#include "netcdf_par.h"
+
+
 #ifndef NcFileClass
 #define NcFileClass
 
@@ -20,19 +23,19 @@ namespace netCDF
    public:
 
       enum FileMode
-	 {
-	    read,	//!< File exists, open read-only.
-	    write,      //!< File exists, open for writing.
-	    replace,	//!< Create new file, even if already exists.
-	    newFile	//!< Create new file, fail if already exists.
-	 };
+     {
+        read,	//!< File exists, open read-only.
+        write,      //!< File exists, open for writing.
+        replace,	//!< Create new file, even if already exists.
+        newFile	//!< Create new file, fail if already exists.
+     };
 
       enum FileFormat
          {
-	    classic,    //!< Classic format, classic data model
-	    classic64,  //!< 64-bit offset format, classic data model
-	    nc4,        //!< (default) netCDF-4/HDF5 format, enhanced data model
-	    nc4classic  //!< netCDF-4/HDF5 format, classic data model
+        classic,    //!< Classic format, classic data model
+        classic64,  //!< 64-bit offset format, classic data model
+        nc4,        //!< (default) netCDF-4/HDF5 format, enhanced data model
+        nc4classic  //!< netCDF-4/HDF5 format, classic data model
          };
 
 
@@ -49,15 +52,32 @@ namespace netCDF
 
 
       /*!
-	Opens a netCDF file.
-	\param filePath    Name of netCDF optional path.
-	\param fMode       The file mode:
-	                    - 'read'    File exists, open for read-only.
-	                    - 'write'   File exists, open for writing.
-	                    - 'replace' Create new file, even it already exists.
-	                    - 'newFile' Create new file, fail it exists already.
+    Opens a netCDF file.
+    \param filePath    Name of netCDF optional path.
+    \param fMode       The file mode:
+                        - 'read'    File exists, open for read-only.
+                        - 'write'   File exists, open for writing.
+                        - 'replace' Create new file, even it already exists.
+                        - 'newFile' Create new file, fail it exists already.
       */
       NcFile(const std::string& filePath, FileMode fMode);
+
+
+      /*!
+        Opens/creates a netCDF file for parallel I/O.
+        Signature based on TE Hilinski's modifications to Lynton Appels code:
+             Thomas E. Hilinski <tom.hilinski@colostate.edu>
+             Natural Resource Ecology Laboratory, Colorado State University
+             IRC <www.nrel.colostate.edu/projects/irc/>
+             Date:  2013Jan
+        Contributed by Alexander Herzig, 2022-01, Manaaki Whenua - Landcare Research NZ
+        \param mpiComm
+        \param mpiInfo
+        \param fileName
+        \param fileMode
+      */
+      NcFile(MPI_Comm& comm, MPI_Info& info, const std::string fileName, const FileMode fMode);
+
 
       /*!
         Create a netCDF file.
@@ -65,6 +85,16 @@ namespace netCDF
         \param ncFileFlags File flags from netcdf.h
       */
       void create(const std::string& filePath, int ncFileFlags);
+
+      /*!
+       * \brief Creates a netCDF file for parallel I/O
+       *        Contributed by Alexander Herzig, 2022-01, Manaaki Whenua - Landcare Research NZ
+       * \param comm
+       * \param info
+       * \param filePath
+       * \param fMode
+       */
+      void create(MPI_Comm& comm, MPI_Info& info, const std::string& fileName, int ncFileFlags);
 
       /*!
         Opens a netCDF file.
@@ -86,11 +116,11 @@ namespace netCDF
       void open(const std::string& filePath, FileMode fMode);
 
       /*!
-	Creates a netCDF file of a specified format.
-	\param filePath    Name of netCDF optional path.
-	\param fMode       The file mode:
-	                    - 'replace' Create new file, even it already exists.
-	                    - 'newFile' Create new file, fail it exists already.
+    Creates a netCDF file of a specified format.
+    \param filePath    Name of netCDF optional path.
+    \param fMode       The file mode:
+                        - 'replace' Create new file, even it already exists.
+                        - 'newFile' Create new file, fail it exists already.
       */
       NcFile(const std::string& filePath, FileMode fMode, FileFormat fFormat);
       /*!
@@ -101,6 +131,19 @@ namespace netCDF
                           - 'newFile' Create new file, fail it exists already.
       */
       void open(const std::string& filePath, FileMode fMode, FileFormat fFormat);
+
+
+      /*!
+       * \brief Opens a netCDF file for parallel I/O. The file format of the underlying
+       *        file is determined automatically.
+       *        Contributed by Alexander Herzig, 2022-01, Manaaki Whenua - Landcare Research NZ
+       * \param mpiComm   MPI communicator of the processes opening this file in parallel
+       * \param mpiInfo   I/O hints  or MPI_INFO_NULL
+       * \param fileName  path to the file
+       * \param fileMode  NcFile::read | NcFile::write
+       */
+
+      void open(MPI_Comm& comm, MPI_Info& info, const std::string fileName, const FileMode fMode);
 
       //! Close a file before destructor call
       void close();
@@ -122,13 +165,13 @@ namespace netCDF
 
 
    private:
-	   /* Do not allow definition of NcFile involving copying any NcFile or NcGroup.
-		  Because the destructor closes the file and releases al resources such
-		  an action could leave NcFile objects in an invalid state */
-	   NcFile& operator =(const NcGroup & rhs);
-	   NcFile& operator =(const NcFile & rhs);
-	   NcFile(const NcGroup& rhs);
-	   NcFile(const NcFile& rhs);
+       /* Do not allow definition of NcFile involving copying any NcFile or NcGroup.
+          Because the destructor closes the file and releases al resources such
+          an action could leave NcFile objects in an invalid state */
+       NcFile& operator =(const NcGroup & rhs);
+       NcFile& operator =(const NcFile & rhs);
+       NcFile(const NcGroup& rhs);
+       NcFile(const NcFile& rhs);
    };
 
 }
