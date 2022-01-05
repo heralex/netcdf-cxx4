d358cd58dcc11949ef945b815d7b1a2a9b941ab0
Alex Herzig <herziga@landcareresearch.co.nz>
2022-01-05 16:38:28 +1300

    enh: ncFile - add ctor and open/create methods for parallel I/O

--- cxx4/ncFile.cpp
+++ cxx4/ncFile.cpp
@@ -54,6 +54,42 @@ NcFile::NcFile(const string& filePath, const int ncFileFlags)
   open(filePath, ncFileFlags);
 }
 
+// constructor that opens/creates a file for parallel I/O
+NcFile::NcFile(MPI_Comm& comm, MPI_Info& info, const std::string fileName, const FileMode fMode)
+{
+    open(comm, info, fileName, fMode);
+}
+
+
+// open a netCDF file for parallel I/O
+void NcFile::open(MPI_Comm& comm, MPI_Info& info, const std::string fileName, const FileMode fMode)
+{
+    if (!nullObject)
+      close();
+
+
+    switch (fMode)
+      {
+      case NcFile::write:
+        ncCheck(nc_open_par(fileName.c_str(), NC_WRITE, comm, info, &myId),__FILE__,__LINE__);
+        break;
+      case NcFile::read:
+        ncCheck(nc_open_par(fileName.c_str(), NC_NOWRITE, comm, info, &myId),__FILE__,__LINE__);
+        break;
+      case NcFile::newFile:
+        ncCheck(nc_create_par(fileName.c_str(), NC_NETCDF4 | NC_NOCLOBBER, comm, info, &myId),__FILE__,__LINE__);
+        break;
+      case NcFile::replace:
+        ncCheck(nc_create_par(fileName.c_str(), NC_NETCDF4 | NC_CLOBBER, comm, info, &myId),__FILE__,__LINE__);
+        break;
+      }
+
+    g_ncid = myId;
+
+    nullObject=false;
+}
+
+
 /*! Open a file from a path and an NC_FLAG
  *
  * This will allow for fine-grained control by the user.
@@ -122,6 +158,18 @@ void NcFile::create(const string& filePath, const int ncFileFlags) {
   nullObject=false;
 }
 
+// create file for paralell I/O
+void NcFile::create(MPI_Comm& comm, MPI_Info& info, const std::string& fileName, int ncFileFlags)
+{
+    if (!nullObject)
+        close();
+
+    ncCheck(nc_create_par(fileName.c_str(), ncFileFlags, comm, info, &myId), __FILE__, __LINE__);
+    g_ncid = myId;
+
+    nullObject = false;
+}
+
 void NcFile::open(const string& filePath, const FileMode fMode, const FileFormat fFormat )
 {
   if (!nullObject)
@@ -131,17 +179,17 @@ void NcFile::open(const string& filePath, const FileMode fMode, const FileFormat
   switch (fFormat)
     {
     case NcFile::classic:
-	format = 0;
-	break;
+    format = 0;
+    break;
     case NcFile::classic64:
-	format = NC_64BIT_OFFSET;
-	break;
+    format = NC_64BIT_OFFSET;
+    break;
     case NcFile::nc4:
-	format = NC_NETCDF4;
-	break;
+    format = NC_NETCDF4;
+    break;
     case NcFile::nc4classic:
-	format = NC_NETCDF4 | NC_CLASSIC_MODEL;
-	break;
+    format = NC_NETCDF4 | NC_CLASSIC_MODEL;
+    break;
     }
   switch (fMode)
     {
